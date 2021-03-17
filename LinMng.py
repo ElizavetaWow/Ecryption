84a20df8a628f3f9eb60bf3e30b3b5a9584f9802
Elizaveta <pigtail2001@gmail.com>
2021-03-18 01:49:15 +0300

    part 1 of linux

--- LinMng.py
+++ LinMng.py
@@ -3,12 +3,61 @@ class LinMng:
     def __init__(self, path):
         """Constructor"""
         self.path = path
+        if os.path.exists(path):
+            os.chdir(path)
+        else:
+            self.path = os.getcwd()
+        self.root = path
 
     def create(self, options):
-        pass
+        if options[0] == "-f":
+            name = ' '.join(options[1:])
+            if not (self.root in name):
+                name = os.path.join(self.path, name)
+            if not os.path.isfile(name):
+                os.system('touch ' + name)
+                return "Ok"
+            else:
+                return "Such file already exists"
+        elif options[0] == "-d":
+            name = ' '.join(options[1:])
+            name.replace("\\", "\\\\")
+            if not (self.root in name):
+                name = os.path.join(self.path, name)
+            if not os.path.isdir(name):
+                os.system('mkdir ' + name)
+                return "Ok"
+            else:
+                return "Such directory already exists"
+        else:
+            return "Wrong input format"
 
     def delete(self, options):
-        pass
+        if options[0] == "-f":
+            name = ' '.join(options[1:])
+            if not (self.root in name):
+                name = os.path.join(self.path, name)
+            if os.path.isfile(name):
+                os.system('rm ' + name)
+                return "Ok"
+            else:
+                return "Such file does not exist"
+        elif options[0] == "-d":
+            name = ' '.join(options[1:])
+            name.replace("\\", "\\\\")
+            if not (self.root in name):
+                name = os.path.join(self.path, name)
+            if os.path.isdir(name):
+                try:
+                    os.system('rmdir ' + name)
+                    return "Ok"
+                except OSError:
+                    return "Directory is not empty"
+
+            else:
+                return "Such directory does not exist"
+        else:
+            return "Wrong input format"
 
     def open(self, options):
         pass
@@ -23,4 +72,26 @@ class LinMng:
         pass
 
     def changeDirectory(self, options):
-        pass
+        name = ' '.join(options)
+        if name == "..":
+            substr = self.path.partition(self.root)[2]
+            if substr.count("\\") == 0:
+                return "It is a root directory"
+            else:
+                os.system('cd ..')
+                self.path = os.getcwd()
+                return "Ok"
+        else:
+            if not (self.root in name):
+                name = os.path.join(self.path, name)
+            if os.path.isdir(name):
+                os.system('cd ' + name)
+                self.path = os.getcwd()
+                self.print_content()
+                return "Ok"
+            else:
+                return "Such directory does not exist"
+
+    def print_content(self):
+        for i in os.listdir(self.path):
+            print(i)
