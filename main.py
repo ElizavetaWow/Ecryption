51f3dc6863479a0c2af27c16b7e40cd20faac214
Elizaveta <pigtail2001@gmail.com>
2021-03-17 21:09:32 +0300

    Added choosing action

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ import WinMng, LinMng
 def mainMenu():
     print("You can do following operations: ")
     print("1.Open \n2.Rename \n3.Move and Paste \n4.Copy and Paste \n5.Delete\n6.Create files\nexit")
-    
+
+def askAction():
+    command = input("Print a command: ").split()
+    if command[0] in menuList:
+        return command
+    else:
+        print("Such command is not found, try one more time")
+        return "Not Found"
 
 
 if __name__ == '__main__':
+    menuList = ["create", "delete", "open", "move", "copy", "rename", "exit"]
     print('Welcome to file manager!\n')
     platformName = platform.system()
     print('Your OS is :', platformName, '.\n')
@@ -15,5 +23,25 @@ if __name__ == '__main__':
         manager = WinMng()
     if platformName == "Linux":
         manager = LinMng()
+    while True:
+        answer = askAction()
+        if answer == "Not Found":
+            continue
+        commandIndex = menuList.index(answer[0])
+        if commandIndex == 0:
+            manager.create(answer[1:])
+        if commandIndex == 1:
+            manager.delete(answer[1:])
+        if commandIndex == 2:
+            manager.open(answer[1:])
+        if commandIndex == 3:
+            manager.move(answer[1:])
+        if commandIndex == 4:
+            manager.copy(answer[1:])
+        if commandIndex == 5:
+            manager.rename(answer[1:])
+        if commandIndex == 6:
+            print("Good bye")
+            break
 
 
