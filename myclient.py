837f9e1f78b8373b7f467580b7b82bc925450780
Elizaveta <pigtail2001@gmail.com>
2021-03-31 23:45:34 +0300

    Added auxiliary functions of getting and sending messages

--- myclient.py
+++ myclient.py
@@ -1,81 +1,58 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import SocketPlus
+# Напишите вспомогательные функции, которые реализуют отправку и принятие текстовых сообщений в сокет.
+# Функция отправки должна дополнять сообщение заголовком фиксированной длины, в котором содержится информация о длине сообщения.
+# Функция принятия должна читать сообщение с учетом заголовка. В дополнении реализуйте преобразование строки в байтовый массив и
+# обратно в этих же функциях.
+# Дополнително оценивается, если эти функции будут реализованы как унаследованное расширение класса socket библиотеки socket.
 
-import socket
+def getAddress():
+    host = input('Write hostname:')
+    socket_number = input('Write socket:')
+    if not host:
+        host = 'localhost'
+    if not socket_number or not socket_number.isdigit():
+        socket_number = 1025
+    else:
+        socket_number = int(socket_number)
+    return host, socket_number
 
+sock = SocketPlus.SocketPlus()
 
-def getData(conn, n):
-    try:
-        data = conn.recv(n)
-        return data, True
-    except ConnectionResetError as e:
-        print(e)
-        print("Lost connection from server\n")
-    except Exception as s:
-        print(s)
-    return "Exception", False
-
-
-def getInput(msg):
-    try:
-        prompt = input(msg)
-        return prompt, True
-    except KeyboardInterrupt as k:
-        print(k)
-        return "Exception", False
-
-
-sock = socket.socket()
-host = getInput('Write hostname:')
-if not (host[1]):
-    print("Stop program")
-    exit()
-socket_number = getInput('Write socket:')
-if not (socket_number[1]):
-    print("Stop program")
-    exit()
-host, socket_number = host[0], socket_number[0]
-if not host:
-    host = 'localhost'
-if not socket_number:
-    socket_number = 1025
-print(f'Connecting to server on port {host}, {socket_number}')
 try:
-    sock.connect((host, int(socket_number)))
+    host, socket_number = getAddress()
+    print(f'Connecting to server on port {host}, {socket_number}')
+    sock.connect((host, socket_number))
+    print('Connection established')
+
+    data = sock.getP()
+    if not data:
+        raise Exception
+    print(data)
+    if 'Hello, what is your name?' in data:
+        name = input()
+        if not name:
+            raise Exception
+        sock.sendP(name)
+
+    msg = "start"
+    while True:
+        msg = input('Write message to server\n')
+        if not msg:
+            break
+        sock.sendP(msg)
+        data = sock.getP()
+        if not data:
+            break
+        print(data)
+    print('End connection with server')
+
+except KeyboardInterrupt as k:
+    print(k)
 except ConnectionRefusedError:
     print("Connection refused")
-    exit()
-except ValueError:
-    print("Invalid socket data. Connection refused")
-    exit()
-
-print('Connection established')
-
-data, status = getData(sock, 1024)
-if not status:
+except Exception as s:
+    print(s)
+finally:
     sock.close()
-    exit()
-if 'what is your name?' in data.decode():
-    name, sts = getInput("Hello, what's your name?\n")
-    if not sts:
-        exit()
-    sock.send(name.encode())
-else:
-    print(data.decode())
-
-s = "start"
-while s != "exit":
-    s, sts = getInput('Write message to server\n')
-    if not sts or not s:
-        break
-    sock.send(s.encode())
-    data, status = getData(sock, 1024)
-    if not status:
-        sock.close()
-        print('End connection with server')
-        exit()
-    print(data.decode())
-sock.send('exit'.encode())
-
-print('End connection with server')
-sock.close()
--- myserver.py
+++ myserver.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import errno
+import errno, socket
 import json
-import socket
+import SocketPlus
 
+def sendP(conn, data):
+    prefix = str(len(data)).zfill(10)
+    conn.send((prefix + data).encode())
 
-def getData(conn, n):
+def getP(conn):
     try:
-        data = conn.recv(n)
-        return data, True
-    except ConnectionResetError:
-        log.write("Lost connection from client\n")
-    except KeyboardInterrupt:
-        log.write("Stop program\n")
-        log.write("Lost connection from client\n")
-    return "Exception", False
-
+        prefix = conn.recv(10)
+        msg_length = int(prefix.decode())
+        data = conn.recv(msg_length)
+        return data.decode()
+    except Exception:
+        return None
 
 try:
     open('clients.json', 'r')
@@ -27,50 +27,50 @@ except FileNotFoundError as e:
 
 with open('log.txt', 'a') as log:
     log.write('Server starts working\n')
-    sock = socket.socket()
-    for i in range(1025, 65536):
-        try:
-            sock.bind(('', i))
-            break
-        except socket.error as e:
-            if e.errno != errno.EADDRINUSE:
-                log.write(e)
-    log.write('Server\'s socket is ' + str(sock.getsockname()[1]) + '\n')
-    while True:
-        log.write('Server listen the socket\n')
-        sock.listen(1)
-        try:
-            conn, addr = sock.accept()
-        except KeyboardInterrupt as k:
-            log.write("Stop program")
-            exit()
-
-        log.write('Connection established:' + str(addr) + '\n')
-        with open('clients.json', 'r') as cl:
-            client_dict = json.load(cl)
-            if client_dict.get(str(addr[1])) != '' and client_dict.get(str(addr[1])) is not None:
-                hello = "Hello, " + str(client_dict.get(str(addr[1])))
-                conn.send(hello.encode())
-            else:
-                conn.send("Hello, what is your name?".encode())
-                name, status = getData(conn, 1024)
-                if not status:
-                    conn.close()
-                    continue
-                client_dict[str(addr[1])] = name.decode()
-        log.write('Server write to json file\n')
-        with open('clients.json', 'w') as cl:
-            json.dump(client_dict, cl)
-        log.write('Server receiving data from client\n')
-        while True:
-            data, status = getData(conn, 1024)
-            if not status or (data == 'exit'.encode()):
+    sock = SocketPlus.SocketPlus()
+    try:
+        for i in range(1025, 65536):
+            try:
+                sock.bind(('', i))
                 break
-            if data:
+            except socket.error as e:
+                if e.errno != errno.EADDRINUSE:
+                    log.write(e)
+        log.write('Server\'s socket is ' + str(sock.getsockname()[1]) + '\n')
+        while True:
+            log.write('Server listen the socket\n')
+            sock.listen(1)
+            conn, addr = sock.accept()
+            log.write('Connection established:' + str(addr) + '\n')
+            with open('clients.json', 'r') as cl:
+                client_dict = json.load(cl)
+                if client_dict.get(str(addr[1])) != '' and client_dict.get(str(addr[1])) is not None:
+                    hello = "Hello, " + str(client_dict.get(str(addr[1])))
+                    sendP(conn, hello)
+                else:
+                    sendP(conn, "Hello, what is your name?")
+                    name = getP(conn)
+                    if not name:
+                        conn.close()
+                        continue
+                    client_dict[str(addr[1])] = name
+            log.write('Server write to json file\n')
+            with open('clients.json', 'w') as cl:
+                json.dump(client_dict, cl)
+            log.write('Server receiving data from client\n')
+            while True:
+                data = getP(conn)
+                if not data or (data == 'exit'):
+                    sendP(conn, '')
+                    break
                 log.write('Server sending data to client\n')
-                conn.send(data.upper())
+                sendP(conn, data.upper())
 
-        log.write('Server stops connection with' + str(addr) + '\n')
-        conn.close()
-
+            log.write('Server stops connection with' + str(addr) + '\n')
+            conn.close()
+    except KeyboardInterrupt as k:
+        log.write(k)
+    except ConnectionResetError:
+        log.write("Lost connection from client\n")
     log.write('Server stops working\n')
+    sock.close()
