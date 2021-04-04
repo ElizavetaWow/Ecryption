22c971ee6da31bddb0e2023aa516b9371d8f4168
Elizaveta <pigtail2001@gmail.com>
2021-04-04 16:50:48 +0300

    scanner with progress bar

--- scanner.py
+++ scanner.py
@@ -1,5 +1,20 @@
 import socket
 import threading
+import sys
+
+def bar(step, n):
+    length = 60
+    filled = int(round(length * step/n))
+    unfilled = length - filled
+    view = "*"*filled+"-"*unfilled
+    percents = round(100.0 * step/n, 1)
+    if step == n - 1:
+        dop = '\n'
+    else:
+        dop = '\r'
+    sys.stdout.write(f'[{view}] {percents}%'+dop)
+    sys.stdout.flush()
+
 
 
 class T(threading.Thread):
@@ -21,11 +36,15 @@ class T(threading.Thread):
 
 lock = threading.Lock()
 n = 2**16 - 1
+
 host = input('Write hostname:')
 soc_list = []
 threads = [T(host, port) for port in range(1, n)]
-[threads[port - 1].start() for port in range(1, n)]
+for port in range(1, n):
+    threads[port - 1].start()
+    bar(port, n)
 [threads[port - 1].join() for port in range(1, n)]
 soc_list.sort()
 for port in soc_list:
     print("Порт", port, "открыт")
+
