fba98fab66e630f9c140e1bc2518524e2bff23a0
Elizaveta <pigtail2001@gmail.com>
2021-04-04 15:18:11 +0300

    parallel scanning (addition 1.i)

--- scanner.py
+++ scanner.py
@@ -1,15 +1,25 @@
 import socket
-from threading import Thread
+import threading
+
+class T(threading.Thread):
+    def __init__(self, host, n):
+        threading.Thread.__init__(self, name="t" + str(n))
+        self.host = host
+        self.port = n
+
+    def run(self):
+        sock = socket.socket()
+        try:
+            sock.connect((self.host, self.port))
+            print("Порт", self.port, "открыт")
+        except:
+            pass
+        sock.close()
 
 n = 2**16 - 1
 host = input('Write hostname:')
 
 for port in range(1, n):
     sock = socket.socket()
-    try:
-        sock.connect((host, port))
-        print("Порт", port, "открыт")
-    except:
-        continue
-    finally:
-        sock.close()
+    p = T(host, port)
+    p.start()
