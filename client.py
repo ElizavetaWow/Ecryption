b35c4460343f851fc87855e2c61bcb92740f3037
Elizaveta <pigtail2001@gmail.com>
2021-04-04 14:44:36 +0300

    Simple threaded server

--- client.py
+++ client.py
@@ -7,9 +7,8 @@ sock.connect(('localhost', 9090))
 
 msg = input()
 sock.send(msg.encode())
-
 data = sock.recv(1024)
-
 sock.close()
 
 print(data.decode())
+
--- server.py
+++ server.py
@@ -1,20 +1,32 @@
 import socket
+import threading
+
+
+class T(threading.Thread):
+    def __init__(self, conn, addr, n):
+        threading.Thread.__init__(self, name="t" + str(n))
+        self.addr = addr
+        self.conn = conn
+
+    def run(self):
+        msg = ''
+        while True:
+            data = self.conn.recv(1024)
+            if not data:
+                break
+            msg += data.decode()
+            self.conn.send(data)
+        print(msg)
+        self.conn.close()
+
 
 sock = socket.socket()
 sock.bind(('localhost', 9090))
 sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
-
+n = 1
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
-
-conn.close()
+    conn, addr = sock.accept()
+    print(addr)
+    p = T(conn, addr, n)
+    p.start()
+    n += 1
