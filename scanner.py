1f07319dc7951db870227158c9d8c0ba589484aa
Elizaveta <pigtail2001@gmail.com>
2021-04-04 15:46:53 +0300

    right order scanner (addition 1.ii)

--- scanner.py
+++ scanner.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 
+
 class T(threading.Thread):
     def __init__(self, host, n):
         threading.Thread.__init__(self, name="t" + str(n))
@@ -8,18 +9,23 @@ class T(threading.Thread):
         self.port = n
 
     def run(self):
+        global soc_list
         sock = socket.socket()
         try:
             sock.connect((self.host, self.port))
-            print("Порт", self.port, "открыт")
+            with lock:
+                soc_list.append(self.port)
         except:
             pass
         sock.close()
 
+lock = threading.Lock()
 n = 2**16 - 1
 host = input('Write hostname:')
-
-for port in range(1, n):
-    sock = socket.socket()
-    p = T(host, port)
-    p.start()
+soc_list = []
+threads = [T(host, port) for port in range(1, n)]
+[threads[port - 1].start() for port in range(1, n)]
+[threads[port - 1].join() for port in range(1, n)]
+soc_list.sort()
+for port in soc_list:
+    print("Порт", port, "открыт")
