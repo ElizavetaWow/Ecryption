868dc50d8bfdc667e6d4837bd60540ab82298045
Elizaveta <pigtail2001@gmail.com>
2021-04-10 14:42:58 +0300

    move class to server file

--- commonFuncs.py
+++ commonFuncs.py
@@ -7,9 +7,10 @@ def sendP(sock, data):
     prefix = str(len(data)).zfill(10)
     sock.send((prefix + data).encode())
 
-def sendAll(socks, data):
-    for sock in socks:
-        sendP(sock, data)
+def sendAll(users, conn, data):
+    for user in users:
+        if user.is_alive() and user.conn != conn:
+            sendP(user.conn, data)
 
 def getP(sock):
     try:
@@ -19,3 +20,13 @@ def getP(sock):
         return data.decode()
     except Exception:
         return None
+
+def createSock(host='localhost'):
+    sock = socket.socket()
+    for i in range(1, 2 ** 16 - 1):
+        try:
+            sock.bind((host, i))
+            print(f"Address: {host} [{i}]")
+            return sock
+        except socket.error:
+            pass
--- myserver.py
+++ myserver.py
@@ -1,21 +1,73 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from user import *
 import socket
 import threading
+import json
 from commonFuncs import *
 
-def createSock(host='localhost'):
-    sock = socket.socket()
-    for i in range(1, 2 ** 16 - 1):
-        try:
-            sock.bind((host, i))
-            print(f"Address: {host} [{i}]")
-            return sock
-        except socket.error:
-            pass
 
+class User(threading.Thread):
+    def __init__(self, conn, addr):
+        threading.Thread.__init__(self, name=str(addr[0]) + " " + str(addr[1]))
+        self.host = addr[0]
+        self.port = addr[1]
+        self.conn = conn
+
+    def run(self):
+        try:
+            if self.authentication():
+                while True:
+                    with lock2:
+                        with open('log.txt', 'a') as log:
+                            log.write('Server sending data to ' + str(self.host) + " " + str(self.port) + '\n')
+                    data = getP(self.conn)
+                    if not data or (data == 'exit'):
+                        sendP(self.conn, '')
+                        break
+                    sendAll(users, self.conn, data.upper())
+        except KeyboardInterrupt as k:
+            with lock2:
+                with open('log.txt', 'a') as log:
+                    log.write(k)
+        except ConnectionResetError:
+            with lock2:
+                with open('log.txt', 'a') as log:
+                    log.write("Lost connection from client\n")
+        with lock2:
+            with open('log.txt', 'a') as log:
+                log.write('Server stops connection with ' + str(self.host) + " " + str(self.port) + '\n')
+        self.conn.close()
+
+    def authentication(self):
+        with lock:
+            with open('clients.json', 'r') as cl:
+                client_dict = json.load(cl)
+            key = str(self.host) + " " + str(self.port)
+            sendP(self.conn, "Enter your name: ")
+            nameEnter = getP(self.conn)
+            sendP(self.conn, "Enter the password: ")
+            passwordEnter = getP(self.conn)
+            result = True
+            if key in client_dict.keys():
+                name, password = client_dict.get(key)
+                if name == nameEnter and password == passwordEnter:
+                    sendP(self.conn, "Login completed")
+                else:
+                    sendP(self.conn, "Login failed. Goodbye!")
+                    result = False
+            else:
+                if not nameEnter or not passwordEnter:
+                    sendP(self.conn, "Wrong input values!")
+                    result = False
+                client_dict[key] = [nameEnter, passwordEnter]
+                sendP(self.conn, "Registration completed")
+            with open('clients.json', 'w') as cl:
+                json.dump(client_dict, cl)
+        return result
+
+
+lock = threading.Lock()
+lock2 = threading.Lock()
 try:
     open('clients.json', 'r')
 except FileNotFoundError as e:
@@ -25,8 +77,8 @@ except FileNotFoundError as e:
 users = list()
 with open('log.txt', 'a') as log:
     log.write('Server starts working\n')
+sock = createSock()
 try:
-    sock = createSock()
     with open('log.txt', 'a') as log:
         log.write('Server\'s socket is ' + str(sock.getsockname()[1]) + '\n')
     while True:
@@ -36,7 +88,6 @@ try:
         with open('log.txt', 'a') as log:
             log.write('Connection established:' + str(addr) + '\n')
         users[-1].start()
-        print(users)
         for u in users:
             if not u.is_alive():
                 users.remove(u)
